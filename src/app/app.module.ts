ec3ab5b15067e37aa7be8d63fdca4312b27febec
Anand-S-L <anandslperoorkada@gmail.com>
2022-08-03 22:36:20 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { ViewComponent } from './view/view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const approute : Routes=[
+  {
+    path : "",
+    component : AddComponent
+  },
+  {
+    path : "search",
+    component : ViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
-    ViewComponent
+    ViewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(approute)
   ],
   providers: [],
   bootstrap: [AppComponent]
